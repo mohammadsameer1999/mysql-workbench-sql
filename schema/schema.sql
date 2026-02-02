9c0abf85cf57420da1e9d34f6cbde336dfa58d09
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2026-02-02 18:05:15 +0530

    Add retrive customer where county name is india

--- schema/schema.sql
+++ schema/schema.sql
@@ -19,3 +19,24 @@ iNSERT INTO CUSTOMER (FIRSTNAME, COUNTRY, SCORE)
 -- Retrive All customer		
 SELECT * FROM CUSTOMER;
 
+CREATE TABLE orders (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  order_date DATE,
+  sales DECIMAL(10,2),
+  CONSTRAINT fk_customer
+    FOREIGN KEY (customer_id)
+    REFERENCES customer(id)
+);
+
+-- Retrive each customers name country score
+SELECT  FIRSTNAME, COUNTRY, SCORE  FROM CUSTOMER;
+
+-- Retrive customer with a score not equal to zero
+SELECT * FROM  CUSTOMER WHERE  SCORE != 91;
+-- Retrive  customer from  India
+SELECT  * 
+FROM CUSTOMER 
+ WHERE COUNTRY = 'INDIA';
+
+
