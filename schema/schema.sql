84553f4219577190fe797cb22b5a8fdd574c92e9
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2026-02-05 18:25:25 +0530

    add new sql question sheet

--- schema/schema.sql
+++ schema/schema.sql
@@ -38,5 +38,35 @@ SELECT * FROM  CUSTOMER WHERE  SCORE != 91;
 SELECT  * 
 FROM CUSTOMER 
  WHERE COUNTRY = 'INDIA';
+ 
+ -- Retrive all customer  and sort the result by the highest score first
+ 
+ SELECT * FROM CUSTOMER ORDER BY SCORE DESC;
+ -- Retrive all customer  and sort the result by the country and then by the highest score first
+ 
+ SELECT * FROM CUSTOMER ORDER BY  SCORE DESC, COUNTRY ASC;
+ 
+ -- Find the total score for each country
+ 
+SELECT COUNTRY, SUM(SCORE) AS TOTAL_SCORE  FROM CUSTOMER GROUP BY COUNTRY ;
 
+-- Find the total score for each country and the total number of  customers for each country
+SELECT COUNTRY,SUM(SCORE) AS TOTAL_SCORE , COUNT(id) AS TOTAL_CUSTOMERS  FROM CUSTOMER GROUP BY COUNTRY;
+
+/* find the average score for each country
+ considering only customers with a score not equal to 0 and 
+return only those countries with an average score greater then 430  */
+ SELECT COUNTRY,
+ AVG(SCORE) AS AVG_SCORE 
+ FROM CUSTOMER 
+ WHERE SCORE != 0
+ GROUP BY COUNTRY  
+ HAVING AVG(SCORE) > 80;
+ ;
+
+
+-- Return unique list for All countries
+SELECT
+   COUNTRY FROM 
+  CUSTOMER;
 
